b87b31012de4e733892c6fdb449e1a5871aa7507
obadyah <ricanthonylee@gmail.com>
2015-01-18 11:12:36 -0500

    fixed fatal error in test repoInitializer

--- Tests/RepositoryInitializer/Entity1RepositoryInitializer.php
+++ Tests/RepositoryInitializer/Entity1RepositoryInitializer.php
@@ -5,10 +5,12 @@ use Designplug\Repository\Tests\Repository\Entity1Repository;
 
 class Entity1RepositoryInitializer extends RepositoryInitializer{
 
-  public function initialize($repository){
+  public function initialize($repository, array $services = array()){
 
     if( ($repository instanceof Entity1Repository) === false)
       throw new \Exception('Repository must be Entity1 Repository');
+
+    parent::initialize($repository, $services);
 
   }
 
